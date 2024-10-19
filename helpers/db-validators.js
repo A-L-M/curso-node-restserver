d2edafabafca028cf5941d071576d87f9b936ce7
Andres <andreslopezmartinez0@gmail.com>
2024-10-19 01:07:06 -0600

    Fin seccion 13 - version 6.0.0

--- helpers/db-validators.js
+++ helpers/db-validators.js
@@ -35,10 +35,25 @@ const existeProducto = async (id = "") => {
   }
 };
 
+/**
+ * Validar colecciones permitidas
+ */
+
+const coleccionesPermitidas = (coleccion = "", colecciones = []) => {
+  const incluida = colecciones.includes(coleccion);
+  if (!incluida) {
+    throw new Error(
+      `La coleccion ${coleccion} no es permitidas, colecciones permitidas: ${colecciones}`
+    );
+  }
+  return true;
+};
+
 module.exports = {
   esRoleValido,
   emailExiste,
   existeUsuarioPorId,
   existeCategoria,
   existeProducto,
+  coleccionesPermitidas,
 };
--- middlewares/index.js
+++ middlewares/index.js
@@ -1,5 +1,11 @@
 const validarJWT = require("../middlewares/validar-jwt");
 const validaRoles = require("../middlewares/validar-roles");
 const validaCampos = require("../middlewares/validar-campos");
+const validarArchivo = require("../middlewares/validar-archivo");
 
-module.exports = { ...validarJWT, ...validaRoles, ...validaCampos };
+module.exports = {
+  ...validarJWT,
+  ...validaRoles,
+  ...validaCampos,
+  ...validarArchivo,
+};
